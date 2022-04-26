5f86eb7b0353bea56eb95fa922a7e67b2525e255
inf <ialhazmim@gmail.com>
2022-04-26 21:55:30 +0300

    client work

--- connection/client.go
+++ connection/client.go
@@ -1,1 +1,16 @@
 package connection
+
+import (
+	"client/bitfield"
+	"client/p2p"
+	"net"
+)
+
+type Client struct {
+	Conn     net.Conn
+	Choked   bool
+	Bitfield bitfield.Bitfield
+	peer     p2p.Peer
+	infoHash [20]byte
+	peerID   [20]byte
+}
