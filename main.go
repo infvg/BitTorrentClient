254cef7e68366ae49e53ca2b591b44e7d6f422ed
Hisham632 <77436699+Hisham632@users.noreply.github.com>
2022-05-13 22:07:07 -0400

    ToDo

--- main.go
+++ main.go
@@ -5,29 +5,21 @@ import (
 	"fmt"
 )
 
-func main() {
-	//const port uint16 = 6925
-	//peerID := [20]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
+/*
+TO DO:
+-Make the tracker url and get all peers
+-Connect to peers by handshake first and check if choke/unchoke
+-Concurrency so it downloads from all trackers/Peers at the same time and with the Interval value given rechecks at those intervals
+*/
 
-	//fmt.Print(os.Stat("[YURI] Kimi no Suizou o Tabetai [BD1080p HEVC FLAC][Dual Audio]  v4.torrent"))
+func main() {
 
 	torrent, err := torrent.ToTorrentFile("[Yameii] Attack on Titan The Final Season - 28 [English Dub] [WEB-DL 1080p] [D3857496].mkv.torrent")
 
 	fmt.Println(torrent.Trackers)
+
 	if err != nil {
 		return
 	}
-	//info2 := torrent.Open("CC_1914_08_31_TheGoodforNothing_archive.torrent")
 
-	//fmt.Println(info2.InfoHash)
-
-	//fmt.Print(" Line ")
-	//fmt.Print(info2.RequestPeers(peerID, port))
-	//fmt.Print(info2.RequestPeers(peerID, port))
-
-	//fmt.Print(info2.RequestPeers(peerID, port))
-
-	//torrent.TorrentFile.Announce
 }
-
-// copy all the methods he has
